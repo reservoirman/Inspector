adb6ddf86f96d077d0beb74b3648ca91f08c1276
Ten-Seng Guh <tg2458@columbia.edu>
2017-08-04 23:34:33 -0700

    first commit!  Only CLI command is 'sample'

---
+++ src/main/java/org/tsg/inspector/AppUiComponent.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2014 Open Networking Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tsg.inspector;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Deactivate;
+//comment this out import org.apache.felix.scr.annotations.Service;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.ReferenceCardinality;
+import org.onosproject.ui.RequestHandler;
+import org.onosproject.ui.UiExtension;
+import org.onosproject.ui.UiExtensionService;
+import org.onosproject.ui.UiMessageHandler;
+import org.onosproject.ui.UiMessageHandlerFactory;
+import org.onosproject.ui.UiView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Skeletal ONOS UI application component.
+ */
+@Component(immediate = true)
+public class AppUiComponent {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected UiExtensionService uiExtensionService;
+
+    // List of application views
+    private final List<UiView> uiViews = ImmutableList.of(
+            new UiView(UiView.Category.OTHER, "sample", "Sample")
+    );
+
+    // Factory for UI message handlers
+    private final UiMessageHandlerFactory messageHandlerFactory =
+            () -> ImmutableList.of(
+                    new AppUiMessageHandler()
+            );
+
+    // Application UI extension
+    protected UiExtension extension = new UiExtension(uiViews, messageHandlerFactory,
+                                                      getClass().getClassLoader());
+
+    @Activate
+    protected void activate() {
+        uiExtensionService.register(extension);
+        log.info("Started");
+    }
+
+    @Deactivate
+    protected void deactivate() {
+        uiExtensionService.unregister(extension);
+        log.info("Stopped");
+    }
+
+    // Application UI message handler
+    private class AppUiMessageHandler extends UiMessageHandler {
+
+        @Override
+        protected Collection<RequestHandler> createRequestHandlers() {
+            return ImmutableSet.of(new SampleRequest());
+        }
+
+        private class SampleRequest extends RequestHandler {
+            public SampleRequest() {
+                super("sampleRequest");
+            }
+
+            @Override
+            public void process(long sid, ObjectNode objectNode) {
+                log.info("We got a message: {}", objectNode);
+            }
+        }
+    }
+
+}
